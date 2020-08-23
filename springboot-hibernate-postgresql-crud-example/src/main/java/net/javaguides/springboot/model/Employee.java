a46b22426781378c90094934dbf29655e9eb8711
burakozcelik5028 <burakozcelik5028@hotmail.com>
2020-08-24 00:28:28 +0100

    springboot-hibernate-postgresql-crud-example

---
+++ springboot-hibernate-postgresql-crud-example/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,71 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firsName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	public Employee() {
+		super();
+	}
+
+	public Employee(String firsName, String lastName, String email) {
+		super();
+		this.firsName = firsName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirsName() {
+		return firsName;
+	}
+
+	public void setFirsName(String firsName) {
+		this.firsName = firsName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
